da3c9f26eb37d611ec3121ff338501c2178526cd
Jojo <jjojo@defi.sucks>
2024-05-24 18:48:02 -0300

    remove: deprecated js output

--- packages/errors/__test__/example.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vitest_1 = require("vitest");
-(0, vitest_1.describe)('exampleHandler', () => {
-    (0, vitest_1.test)('Sample exampleHandler test', () => {
-        (0, vitest_1.expect)(true).toBe(true);
-    });
-});
