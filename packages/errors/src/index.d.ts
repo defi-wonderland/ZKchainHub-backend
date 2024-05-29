2e7830a66eae0075537e9795e128dd23562d4e19
0xkenj1 <kenji@defi.sucks>
2024-05-28 22:07:25 -0300

    fix: remove unnecesary files

--- packages/errors/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare abstract class BaseError extends Error {
-    readonly name: string;
-    readonly description: string;
-    constructor({ name, description }: { name: string; description: string });
-    getDescription(): string;
-}
-//# sourceMappingURL=index.d.ts.map
