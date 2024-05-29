f8474add248b4ada4a8598bdb19550c9ada98427
0xkenj1 <kenji@defi.sucks>
2024-05-28 22:14:22 -0300

    fix: errors linting

--- packages/serverless/src/errors.ts
+++ packages/serverless/src/errors.ts
@@ -1,10 +1,7 @@
-import axios from "axios";
-
 import { BaseError } from "@zkchainhub/errors";
 
 export default class LambdaError extends BaseError {
     constructor(description: string) {
         super({ name: "LambdaError", description });
-        axios.create();
     }
 }
