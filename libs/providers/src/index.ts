51f834f68e744b00bb7316b595e496c2c7e50680
nigiri <168690269+0xnigir1@users.noreply.github.com>
2024-07-19 01:39:59 -0300

    feat: viem wrapper with readContract and base methods

--- libs/providers/src/index.ts
+++ libs/providers/src/index.ts
@@ -1,2 +1,4 @@
+export * from "./interfaces";
+export * from "./providers";
 export * from "./providers.module";
 export * from "./evmProvider.service";
--- libs/providers/src/providers.module.ts
+++ libs/providers/src/providers.module.ts
@@ -1,13 +1,14 @@
 import { Module } from "@nestjs/common";
 
 import { EvmProviderService } from "./evmProvider.service";
+import { ViemProviderService } from "./providers";
 
 /**
  * Module for managing provider services.
  * This module exports Services for interacting with EVM-based blockchains.
  */
 @Module({
-    providers: [EvmProviderService],
-    exports: [EvmProviderService],
+    providers: [EvmProviderService, ViemProviderService],
+    exports: [EvmProviderService, ViemProviderService],
 })
 export class ProvidersModule {}
