e503a936ac7b1a697a462e196984f04de363ecf8
nigiri <168690269+0xnigir1@users.noreply.github.com>
2024-07-26 15:29:38 -0300

    feat: add pricing service interface

--- libs/pricing/src/services/coingecko.service.ts
+++ libs/pricing/src/services/coingecko.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import { IPricingService } from "@packages/pricing/interfaces";
 
 @Injectable()
-export class CoingeckoService {
+export class CoingeckoService implements IPricingService {
     private readonly API_BASE_URL = "https://api.coingecko.com/api/v3/";
     constructor(private readonly apiKey: string) {}
+
+    async getTokenPrices(
+        _tokenIds: string[],
+        _config: { currency: string } = { currency: "usd" },
+    ): Promise<Record<string, number>> {
+        throw new Error("Method not implemented.");
+    }
 }
