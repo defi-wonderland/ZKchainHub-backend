3109887cc240b3e56cd6923d085062aaafea968b
0xkenj1 <kenji@defi.sucks>
2024-07-31 02:15:04 -0300

    feat: init L1MetricsService

--- libs/metrics/src/l1/l1MetricsService.ts
+++ libs/metrics/src/l1/l1MetricsService.ts
@@ -1,11 +1,27 @@
 import { Injectable } from "@nestjs/common";
 
+import { bridgeHubAbi, sharedBridgeAbi } from "@zkchainhub/metrics/l1/abis";
+import { IPricingService } from "@zkchainhub/pricing";
 import { EvmProviderService } from "@zkchainhub/providers";
+import { AbiWithAddress, ChainId, L1_CONTRACTS } from "@zkchainhub/shared";
 
 /**
  * Acts as a wrapper around Viem library to provide methods to interact with an EVM-based blockchain.
  */
 @Injectable()
 export class L1MetricsService {
-    constructor(private readonly evmProviderService: EvmProviderService) {}
+    private readonly bridgeHub: Readonly<AbiWithAddress> = {
+        abi: bridgeHubAbi,
+        address: L1_CONTRACTS.BRIDGE_HUB,
+    };
+    private readonly sharedBridge: Readonly<AbiWithAddress> = {
+        abi: sharedBridgeAbi,
+        address: L1_CONTRACTS.SHARED_BRIDGE,
+    };
+    private readonly diamondContracts: Map<ChainId, AbiWithAddress> = new Map();
+
+    constructor(
+        private readonly evmProviderService: EvmProviderService,
+        private readonly pricingService: IPricingService,
+    ) {}
 }
