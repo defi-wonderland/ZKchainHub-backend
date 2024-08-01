7338797c9c78b52d0b59ef5b5f9954f6c0d66dd0
nigiri <168690269+0xnigir1@users.noreply.github.com>
2024-08-01 10:24:42 -0300

    fix: pr comments, vscode fmt configuration & build script

--- contracts/src/contracts/TokenBalances.sol
+++ contracts/src/contracts/TokenBalances.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.0;
 
-import {IERC20} from "interfaces/IERC20.sol";
+import {IERC20} from 'interfaces/IERC20.sol';
 
 /**
  * @title TokenBalances
@@ -14,37 +14,37 @@ import {IERC20} from "interfaces/IERC20.sol";
  *
  */
 contract TokenBalances {
-    /// @notice Returns the balance of the target contract for the specified tokens and ether
-    /// @param _targetAddress The target contract address to query balances
-    /// @param _tokenAddresses The token addresses
-    constructor(address _targetAddress, address[] memory _tokenAddresses) {
-        uint256 length = _tokenAddresses.length;
+  /// @notice Returns the balance of the target contract for the specified tokens and ether
+  /// @param _targetAddress The target contract address to query balances
+  /// @param _tokenAddresses The token addresses
+  constructor(address _targetAddress, address[] memory _tokenAddresses) {
+    uint256 length = _tokenAddresses.length;
 
-        // one additional element for the ether balance
-        uint256[] memory returnData = new uint256[](length + 1);
+    // one additional element for the ether balance
+    uint256[] memory returnData = new uint256[](length + 1);
 
-        uint256 i; //saves gas as default is 0
-        while (i < length) {
-            IERC20 token = IERC20(_tokenAddresses[i]);
+    uint256 i; //saves gas as default is 0
+    while (i < length) {
+      IERC20 token = IERC20(_tokenAddresses[i]);
 
-            uint256 balance = token.balanceOf(_targetAddress);
-            returnData[i] = balance;
+      uint256 balance = token.balanceOf(_targetAddress);
+      returnData[i] = balance;
 
-            unchecked {
-                ++i;
-            }
-        }
-        returnData[i] = _targetAddress.balance;
-
-        // encode return data
-        bytes memory data = abi.encode(returnData);
-
-        // force constructor return via assembly
-        assembly {
-            // Return from the start of the data (discarding the original data address)
-            // up to the end of the memory used
-            let dataStart := add(data, 32) // abi.encode adds an additional offset
-            return(dataStart, sub(msize(), dataStart))
-        }
+      unchecked {
+        ++i;
+      }
     }
+    returnData[i] = _targetAddress.balance;
+
+    // encode return data
+    bytes memory data = abi.encode(returnData);
+
+    // force constructor return via assembly
+    assembly {
+      // Return from the start of the data (discarding the original data address)
+      // up to the end of the memory used
+      let dataStart := add(data, 32) // abi.encode adds an additional offset
+      return(dataStart, sub(msize(), dataStart))
+    }
+  }
 }
--- contracts/src/interfaces/IERC20.sol
+++ contracts/src/interfaces/IERC20.sol
@@ -2,8 +2,8 @@
 pragma solidity 0.8.0;
 
 interface IERC20 {
-    /**
-     * @dev Returns the value of tokens owned by `account`.
-     */
-    function balanceOf(address account) external view returns (uint256);
+  /**
+   * @dev Returns the value of tokens owned by `account`.
+   */
+  function balanceOf(address account) external view returns (uint256);
 }
