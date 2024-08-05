27a2ea0ee83af107ecf889bca97aad0359236f47
nigiri <168690269+0xnigir1@users.noreply.github.com>
2024-08-05 11:47:37 -0300

    test: add unit tests

--- libs/metrics/test/unit/l1/l1MetricsService.spec.ts
+++ libs/metrics/test/unit/l1/l1MetricsService.spec.ts
@@ -1,21 +1,23 @@
+import { createMock } from "@golevelup/ts-jest";
 import { Logger } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
 import { WINSTON_MODULE_PROVIDER } from "nest-winston";
+import { encodeFunctionData, erc20Abi, parseEther, zeroAddress } from "viem";
 
+import { L1ProviderException } from "@zkchainhub/metrics/exceptions/provider.exception";
 import { L1MetricsService } from "@zkchainhub/metrics/l1/";
 import { bridgeHubAbi, sharedBridgeAbi } from "@zkchainhub/metrics/l1/abis";
 import { IPricingService, PRICING_PROVIDER } from "@zkchainhub/pricing";
 import { EvmProviderService } from "@zkchainhub/providers";
-import { L1_CONTRACTS } from "@zkchainhub/shared";
+import { L1_CONTRACTS, vitalikAddress } from "@zkchainhub/shared";
+import { ETH, WETH } from "@zkchainhub/shared/tokens/tokens";
 
 // Mock implementations of the dependencies
-const mockEvmProviderService = {
-    // Mock methods and properties as needed
-};
+const mockEvmProviderService = createMock<EvmProviderService>();
 
-const mockPricingService = {
-    // Mock methods and properties as needed
-};
+const mockPricingService = createMock<IPricingService>();
+
+const ONE_ETHER = parseEther("1");
 
 export const mockLogger: Partial<Logger> = {
     log: jest.fn(),
@@ -34,11 +36,15 @@ describe("L1MetricsService", () => {
                 {
                     provide: L1MetricsService,
                     useFactory: (
-                        evmProviderService: EvmProviderService,
-                        pricingService: IPricingService,
+                        mockEvmProviderService: EvmProviderService,
+                        mockPricingService: IPricingService,
                         logger: Logger,
                     ) => {
-                        return new L1MetricsService(evmProviderService, pricingService, logger);
+                        return new L1MetricsService(
+                            mockEvmProviderService,
+                            mockPricingService,
+                            logger,
+                        );
                     },
                     inject: [EvmProviderService, PRICING_PROVIDER, WINSTON_MODULE_PROVIDER],
                 },
@@ -58,6 +64,10 @@ describe("L1MetricsService", () => {
         }).compile();
 
         l1MetricsService = module.get<L1MetricsService>(L1MetricsService);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
     });
 
     describe("constructor", () => {
@@ -106,9 +116,151 @@ describe("L1MetricsService", () => {
     });
 
     describe("ethGasInfo", () => {
-        it("return ethGasInfo", async () => {
+        it("returns gas information from L1", async () => {
+            // Mock the necessary dependencies
+            const mockEstimateGas = jest.spyOn(mockEvmProviderService, "estimateGas");
+            mockEstimateGas.mockResolvedValueOnce(BigInt(21000)); // ethTransferGasCost
+            mockEstimateGas.mockResolvedValueOnce(BigInt(65000)); // erc20TransferGasCost'
+            const mockGetGasPrice = jest.spyOn(mockEvmProviderService, "getGasPrice");
+            mockGetGasPrice.mockResolvedValueOnce(BigInt(50000000000)); // gasPrice
+
+            const mockGetTokenPrices = jest.spyOn(mockPricingService, "getTokenPrices");
+            mockGetTokenPrices.mockResolvedValueOnce({ [ETH.coingeckoId]: 2000 }); // ethPriceInUsd
+
+            // Call the method
             const result = await l1MetricsService.ethGasInfo();
-            expect(result).toEqual({ gasPrice: 50, ethTransfer: 21000, erc20Transfer: 65000 });
+
+            // Assertions
+            expect(mockEstimateGas).toHaveBeenCalledTimes(2);
+            expect(mockEstimateGas).toHaveBeenNthCalledWith(1, {
+                account: vitalikAddress,
+                to: zeroAddress,
+                value: ONE_ETHER,
+            });
+            expect(mockEstimateGas).toHaveBeenNthCalledWith(2, {
+                account: vitalikAddress,
+                to: WETH.contractAddress,
+                data: encodeFunctionData({
+                    abi: erc20Abi,
+                    functionName: "transfer",
+                    args: [L1_CONTRACTS.SHARED_BRIDGE, ONE_ETHER],
+                }),
+            });
+
+            expect(mockGetGasPrice).toHaveBeenCalledTimes(1);
+
+            expect(mockGetTokenPrices).toHaveBeenCalledTimes(1);
+            expect(mockGetTokenPrices).toHaveBeenCalledWith([ETH.coingeckoId]);
+
+            expect(result).toEqual({
+                gasPriceInGwei: 50,
+                ethPrice: 2000,
+                ethTransferGas: 21000,
+                erc20TransferGas: 65000,
+            });
+        });
+
+        it("returns gas information from L1 without ether price", async () => {
+            const mockEstimateGas = jest.spyOn(mockEvmProviderService, "estimateGas");
+            mockEstimateGas.mockResolvedValueOnce(BigInt(21000)); // ethTransferGasCost
+            mockEstimateGas.mockResolvedValueOnce(BigInt(65000)); // erc20TransferGasCost
+
+            const mockGetGasPrice = jest.spyOn(mockEvmProviderService, "getGasPrice");
+            mockGetGasPrice.mockResolvedValueOnce(BigInt(50000000000)); // gasPrice
+
+            const mockGetTokenPrices = jest.spyOn(mockPricingService, "getTokenPrices");
+            mockGetTokenPrices.mockRejectedValueOnce(new Error("Failed to get token prices"));
+
+            const result = await l1MetricsService.ethGasInfo();
+
+            // Assertions
+            expect(result).toEqual({
+                gasPriceInGwei: 50,
+                ethPrice: undefined,
+                ethTransferGas: 21000,
+                erc20TransferGas: 65000,
+            });
+            expect(mockEstimateGas).toHaveBeenCalledTimes(2);
+            expect(mockEstimateGas).toHaveBeenNthCalledWith(1, {
+                account: vitalikAddress,
+                to: zeroAddress,
+                value: ONE_ETHER,
+            });
+            expect(mockEstimateGas).toHaveBeenNthCalledWith(2, {
+                account: vitalikAddress,
+                to: WETH.contractAddress,
+                data: encodeFunctionData({
+                    abi: erc20Abi,
+                    functionName: "transfer",
+                    args: [L1_CONTRACTS.SHARED_BRIDGE, ONE_ETHER],
+                }),
+            });
+
+            expect(mockGetGasPrice).toHaveBeenCalledTimes(1);
+
+            expect(mockGetTokenPrices).toHaveBeenCalledTimes(1);
+            expect(mockGetTokenPrices).toHaveBeenCalledWith([ETH.coingeckoId]);
+        });
+
+        it("throws L1ProviderException when estimateGas fails", async () => {
+            // Mock the necessary dependencies
+            const mockEstimateGas = jest.spyOn(mockEvmProviderService, "estimateGas");
+            mockEstimateGas.mockRejectedValueOnce(new Error("Failed to estimate gas"));
+
+            const mockGetGasPrice = jest.spyOn(mockEvmProviderService, "getGasPrice");
+            mockGetGasPrice.mockResolvedValueOnce(BigInt(50000000000)); // gasPrice
+
+            const mockGetTokenPrices = jest.spyOn(mockPricingService, "getTokenPrices");
+            mockGetTokenPrices.mockResolvedValueOnce({ [ETH.coingeckoId]: 2000 }); // ethPriceInUsd
+
+            // Call the method and expect it to throw L1ProviderException
+            await expect(l1MetricsService.ethGasInfo()).rejects.toThrow(L1ProviderException);
+
+            // Assertions
+            expect(mockEstimateGas).toHaveBeenCalledWith({
+                account: vitalikAddress,
+                to: zeroAddress,
+                value: ONE_ETHER,
+            });
+
+            expect(mockGetTokenPrices).not.toHaveBeenCalled();
+        });
+
+        it("throws L1ProviderException when getGasPrice fails", async () => {
+            // Mock the necessary dependencies
+            const mockEstimateGas = jest.spyOn(mockEvmProviderService, "estimateGas");
+            mockEstimateGas.mockResolvedValueOnce(BigInt(21000)); // ethTransferGasCost
+            mockEstimateGas.mockResolvedValueOnce(BigInt(65000)); // erc20TransferGasCost
+
+            const mockGetGasPrice = jest.spyOn(mockEvmProviderService, "getGasPrice");
+            mockGetGasPrice.mockRejectedValueOnce(new Error("Failed to get gas price"));
+
+            const mockGetTokenPrices = jest.spyOn(mockPricingService, "getTokenPrices");
+            mockGetTokenPrices.mockResolvedValueOnce({ [ETH.coingeckoId]: 2000 }); // ethPriceInUsd
+
+            // Call the method and expect it to throw L1ProviderException
+            await expect(l1MetricsService.ethGasInfo()).rejects.toThrow(L1ProviderException);
+
+            // Assertions
+            expect(mockEstimateGas).toHaveBeenCalledTimes(2);
+            expect(mockEstimateGas).toHaveBeenNthCalledWith(1, {
+                account: vitalikAddress,
+                to: zeroAddress,
+                value: ONE_ETHER,
+            });
+            expect(mockEstimateGas).toHaveBeenNthCalledWith(2, {
+                account: vitalikAddress,
+                to: WETH.contractAddress,
+                data: encodeFunctionData({
+                    abi: erc20Abi,
+                    functionName: "transfer",
+                    args: [L1_CONTRACTS.SHARED_BRIDGE, ONE_ETHER],
+                }),
+            });
+
+            expect(mockGetGasPrice).toHaveBeenCalledTimes(1);
+
+            expect(mockGetTokenPrices).not.toHaveBeenCalled();
         });
     });
 
