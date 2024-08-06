c0cb5bcc8be24023f37b33bc64b1840498d4b9c7
nigiri <168690269+0xnigir1@users.noreply.github.com>
2024-08-06 20:00:29 -0300

    feat: tvl by chain id using multicall

---
+++ libs/shared/src/constants/addresses.ts
@@ -0,0 +1,5 @@
+import { Address } from "abitype";
+
+// This is the address given to native ETH on L2 chains.
+// See: https://github.com/matter-labs/era-contracts/blob/8a70bbbc48125f5bde6189b4e3c6a3ee79631678/l1-contracts/contracts/common/Config.sol#L105
+export const ETH_TOKEN_ADDRESS: Address = "0x0000000000000000000000000000000000000001";
