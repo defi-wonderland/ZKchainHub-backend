09d6590f5d36785cba24ed8cb1d8cf0ed2aead21
0xkenj1 <kenji@defi.sucks>
2024-08-15 16:27:20 -0300

    feat: allow any origin

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,6 +8,12 @@ async function bootstrap() {
     const PORT = process.env.PORT || 3000;
     const app = await NestFactory.create(ApiModule);
     app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+    app.enableCors({
+        origin: "*", // Replace with the origin you want to allow
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true,
+    });
+
     setupOpenApiConfiguration(app);
 
     const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
