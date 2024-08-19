1fd0604769bed7eb81f707b66d047576ac61ac02
nigiri <168690269+0xnigir1@users.noreply.github.com>
2024-08-18 21:21:29 -0300

    feat: pricing package refactor

---
+++ packages/pricing/src/external.ts
@@ -0,0 +1,3 @@
+export type { IPricingService, RateLimitExceeded, ApiNotAvailable } from "./internal.js";
+
+export { CoingeckoService } from "./internal.js";
--- packages/shared/src/internal.ts
+++ packages/shared/src/internal.ts
@@ -3,3 +3,4 @@ export * from "./constants/index.js";
 export * from "./utils/index.js";
 export * from "./metadata/index.js";
 export * from "./logger/index.js";
+export * from "./cache/index.js";
