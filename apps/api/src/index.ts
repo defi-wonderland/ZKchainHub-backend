0e6c40b311675af2bfa5747d9c6abb17d173106e
nigiri <168690269+0xnigir1@users.noreply.github.com>
2024-08-21 12:53:56 -0300

    feat: remove container and add cache-manager lib for in-memory-cache

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,16 +1,45 @@
 import { inspect } from "util";
+import { caching } from "cache-manager";
 
-import { ILogger } from "@zkchainhub/shared";
+import { L1MetricsService } from "@zkchainhub/metrics";
+import { CoingeckoService } from "@zkchainhub/pricing";
+import { EvmProviderService } from "@zkchainhub/providers";
+import { Logger } from "@zkchainhub/shared";
 
 import { App } from "./app.js";
 import { config } from "./common/config/index.js";
-import { Container } from "./container.js";
+import { MetricsController, MetricsRouter } from "./metrics/index.js";
 
-const container = new Container(config);
-const logger = container.get<ILogger>("ILogger");
+const logger = Logger.getInstance();
 
 const main = async (): Promise<void> => {
-    const app = new App(config, [container.get("MetricsRouter")], logger);
+    const memoryCache = await caching("memory", {
+        max: 150, // maximum number of items to store
+        ttl: config.pricing.cacheOptions.ttl * 1000 /*milliseconds*/,
+    });
+
+    const evmProvider = new EvmProviderService(config.l1.rpcUrls, config.l1.chain, logger);
+    const pricingProvider = new CoingeckoService(
+        {
+            apiBaseUrl: config.pricing.pricingOptions.apiBaseUrl,
+            apiKey: config.pricing.pricingOptions.apiKey,
+            apiType: config.pricing.pricingOptions.apiType,
+        },
+        memoryCache,
+        logger,
+    );
+    const l1MetricsService = new L1MetricsService(
+        config.bridgeHubAddress,
+        config.sharedBridgeAddress,
+        config.stateTransitionManagerAddresses,
+        evmProvider,
+        pricingProvider,
+        logger,
+    );
+    const metricsController = new MetricsController(l1MetricsService, logger);
+    const metricsRouter = new MetricsRouter(metricsController, logger);
+
+    const app = new App(config, [metricsRouter], logger);
 
     app.listen();
 };
