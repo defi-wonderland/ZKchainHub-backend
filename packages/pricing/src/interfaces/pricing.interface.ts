b966a2fe29ebc4fcef4274ce1387adba5d8ddf5d
nigiri <168690269+0xnigir1@users.noreply.github.com>
2024-08-27 10:53:17 -0300

    feat: refactor coingecko service to accept address as input

--- packages/pricing/src/interfaces/pricing.interface.ts
+++ packages/pricing/src/interfaces/pricing.interface.ts
@@ -1,16 +1,18 @@
+import { Address } from "@zkchainhub/shared";
+
 // providers
 export type PricingProvider = "coingecko";
 
 /**
  * Represents a pricing service that retrieves token prices.
+ * @dev is service responisility to map address to their internal ID
+ * @dev for native token (eg. ETH), use the one address
  */
 export interface IPricingProvider {
     /**
      * Retrieves the prices of the specified tokens.
-     * @param tokenIds - An array of token IDs.
-     * @returns A promise that resolves to a record containing the token IDs as keys and their corresponding prices as values.
+     * @param addresses - An array of token addresses.
+     * @returns A promise that resolves to a record containing the token address as keys and their corresponding prices or undefined as values
      */
-    getTokenPrices<TokenId extends string = string>(
-        tokenIds: TokenId[],
-    ): Promise<Record<string, number>>;
+    getTokenPrices(addresses: Address[]): Promise<Record<Address, number | undefined>>;
 }
